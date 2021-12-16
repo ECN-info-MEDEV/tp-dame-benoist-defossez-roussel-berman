2652df231e5c2b602d6d83426f5f87847fe4e068
alan <alanroussel@free.fr>
2021-12-16 11:45:07 +0100

    nbrPion + method tourDeJeu

--- src/main/java/ds/jeudame/Joueur.java
+++ src/main/java/ds/jeudame/Joueur.java
@@ -5,11 +5,35 @@
  */
 package ds.jeudame;
 
+import java.util.Scanner;
+
 /**
  *
  * @author alanroussel
  */
 public class Joueur {
     private String nom;
-    
+    private Scanner scanner; 
+
+    public Joueur() {
+        System.out.println("[ENTRÉE]" + " Tapez votre pseudo :");
+        String nomJoueur = scanner.next();
+        this.nom = nomJoueur;
+    }
+    public void tourDeJeu(Echiquier echiquier){
+        System.out.println("[ENTRÉE]"  + " Quel pion souhaites-tu bouger (x) ?  :");
+        int xCurrent = scanner.nextInt();
+        System.out.println("[ENTRÉE]"  + " Quel pion souhaites-tu bouger (y) ? :");
+        int yCurrent = scanner.nextInt();
+        
+        boolean positionAccepted = false;
+        while(!positionAccepted){
+            System.out.println("[ENTRÉE]"  + " Où souhaites-tu aller (x) ? :");
+            int xNew = scanner.nextInt();
+            System.out.println("[ENTRÉE]"  + " Où souhaites-tu aller (y) ?");
+            int yNew = scanner.nextInt();
+            // positionAccepted = echiquier.deplacementautorise(xCurrent, yCurrent, xNew, yNew);
+        }
+        // echiquier.deplacerPion(xCurrent, yCurrent, xNew, yNew);
+    }
 }
