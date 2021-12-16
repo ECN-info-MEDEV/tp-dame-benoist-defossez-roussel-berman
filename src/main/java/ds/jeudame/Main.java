5883cfbbe0e6c4555ff4f35dbf1e2a6aea79bd3a
Quentin Benoist <benoist.quentin@yahoo.fr>
2021-12-16 11:35:14 +0100

    Ajout de fonction

--- src/main/java/ds/jeudame/Main.java
+++ src/main/java/ds/jeudame/Main.java
@@ -11,7 +11,22 @@ package ds.jeudame;
  */
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World ! ");
-        
+        Echiquier echiquier = new Echiquier();
+        Joueur joueur1= new Joueur();
+        Joueur joueur2= new Joueur();
+        boolean victoire=false;
+        int n=0;
+        while(!victoire){
+            if(n%2==0){
+                joueur1.tourDeJeu(echiquier);
+                n=1;
+            }
+            else{
+                joueur2.tourDeJeu(echiquier);
+                n=0;
+            }
+            victoire=echiquier.victoire();
+            //n=n%2+1;
+        }
     }
 }
