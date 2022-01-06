552e1346faae5877459d61534ac44cf97606a448
alan <alanroussel@free.fr>
2022-01-06 09:05:36 +0100

    [sonar] | and & issues

--- src/main/java/ds/jeudame/Echiquier.java
+++ src/main/java/ds/jeudame/Echiquier.java
@@ -51,12 +51,12 @@ public class Echiquier {
     
     public boolean deplacementAutorise(int x1, int y1, int x2, int y2, boolean couleur) {
         //verifie que les coordonnées sont comprises entre 0 et 9
-        if ( x1<0 | x1>9 | x2<0 | x2>9 | y1<0 | y1>9 | y2<0 | y2>9  ) {
+        if ( x1<0 || x1>9 || x2<0 || x2>9 || y1<0 || y1>9 || y2<0 || y2>9  ) {
             return false;
         }
         
         //vérifie si un pion est sur la case de départ et que  la case d'arrivé est vide
-        if (this.plateau[x1][y1] == null | this.plateau[x2][y2] != null) {
+        if (this.plateau[x1][y1] == null || this.plateau[x2][y2] != null) {
             return false;
         }
         
@@ -73,7 +73,7 @@ public class Echiquier {
             }
             //cas déplacement avec prise de pion
             if (y2 == y1+2) {
-                return (2==abs(x1-x2) & this.plateau[(x1+x2)/2][(y1+y2)/2].isWhite());
+                return (2==abs(x1-x2) && this.plateau[(x1+x2)/2][(y1+y2)/2].isWhite());
             }
         }
         
@@ -85,7 +85,7 @@ public class Echiquier {
             }
             //cas déplacement avec prise de pion
             if (y1 == y2+2) {
-                return (2==abs(x1-x2) & !this.plateau[(x1+x2)/2][(y1+y2)/2].isWhite());
+                return (2==abs(x1-x2) && !this.plateau[(x1+x2)/2][(y1+y2)/2].isWhite());
             }
         }        
         return false;
